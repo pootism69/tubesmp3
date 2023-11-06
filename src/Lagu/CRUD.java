9e33fb35db031930f77160dfc3cbac3ccc7e58e4
unknown <imranzulkarnaen46@gmail.com>
2023-11-06 11:24:54 +0700

    Interface apdet

---
+++ src/Lagu/CRUD.java
@@ -0,0 +1,7 @@
+package Lagu;
+public abstract interface CRUD {
+    public abstract void Create();
+    public abstract void Update();
+    public abstract void Read();
+    public abstract void Delete();
+}
---
+++ src/Lagu/Tombol.java
@@ -0,0 +1,9 @@
+package Lagu;
+public abstract interface Tombol {
+   public abstract void Memutar();
+   public abstract void Menjeda();
+   public abstract void Mengulang();
+   public abstract void Menghentikan();
+   public abstract void MemutarSelanjutnya();
+   public abstract void MemutarSebelumnya();
+}
