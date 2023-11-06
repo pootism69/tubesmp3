7b8989661e1242523f896d83c4fb94ab4a443062
unknown <imranzulkarnaen46@gmail.com>
2023-11-06 11:12:46 +0700

    Apdet Baru Kelas Admin

---
+++ src/User/Admin.java
@@ -0,0 +1,34 @@
+package User;
+public class Admin {
+    private String nama;
+    private String sandi;
+    private int id;
+
+    public Admin(){}
+    
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getSandi() {
+        return sandi;
+    }
+
+    public void setSandi(String sandi) {
+        this.sandi = sandi;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    
+}
