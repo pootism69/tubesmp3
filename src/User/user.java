236ca575da12a74f45557a85a56a6948b3aab7b7
Fawwaz Yasirah <fawwazyasirah@gmail.com>
2023-11-06 11:20:38 +0700

    Update Class User

---
+++ src/User/user.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package User;
+
+/**
+ *
+ * @author ASUS
+ */
+public class user {
+    private String nama;
+    private String sandi;
+    private int id;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getSandi() {
+        return sandi;
+    }
+
+    public void setSandi(String sandi) {
+        this.sandi = sandi;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+    
+}
